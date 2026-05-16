251d48a85fbdf284c0a5f1a0fba0b11f1868729b
chyfnuonuo <chengyoufu@gmail.com>
2026-05-16 21:05:36 +0800

    feat: PWA support for poetry quiz simulator
    Add manifest.json, service worker, and icons for PWA installability.
    All features gracefully degrade when opened from file:// protocol.

---
+++ sw.js
@@ -0,0 +1,24 @@
+var CACHE_NAME = 'poetry-quiz-v1';
+var urlsToCache = [
+    '.',
+    'index.html',
+    'manifest.json',
+    'icon-192.png',
+    'icon-512.png'
+];
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function(cache) {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
+});
